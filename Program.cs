728181af0c47a01647c8ce7344d490c303b3d9a8
VSSemakin <VitalSSemakin@yandex.ru>
2026-04-09 17:59:16 +0500

    добавлены методы сложения, вычитания, умножения

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 using System;
 class Program
 {
+    O references
+    static int Add(int a, int b)
+    { return a + b; }
+
+    0 references
+    static int Subtract(int a, int b)
+    { return a - b; }
+
+    O references
+    static int Multiply(int a, int b)
+    { return a * b; }
+
+    O references
     static void Main(string[] args)
     {
         // VERSION: 1.0.0
