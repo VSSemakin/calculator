6bcc7cb0d2d1a5d8646ebc52ae51cf7d595f6811
VSSemakin <VitalSSemakin@yandex.ru>
2026-04-09 18:23:04 +0500

    Убран лишний текст

--- Program.cs
+++ Program.cs
@@ -1,19 +1,16 @@
 using System;
 class Program
 {
-    O references
+
     static int Add(int a, int b)
     { return a + b; }
 
-    0 references
     static int Subtract(int a, int b)
     { return a - b; }
 
-    O references
     static int Multiply(int a, int b)
     { return a * b; }
 
-    O references
     static void Main(string[] args)
     {
         // VERSION: 2.0.0-FINAL
